50b2e8e3f16852c536a52819f7ac00fe1824bad9
Pankaj Mallick <pankaj.mallick@fglife.com>
2017-07-06 22:04:45 -0400

    Updated

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,11 +3,13 @@ import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
 import { HitmeDirective } from './hitme.directive/hitme.directive';
+import { HitcounterComponent } from './hitcounter/hitcounter.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    HitmeDirective
+    HitmeDirective,
+    HitcounterComponent
   ],
   imports: [
     BrowserModule
---
+++ src/app/hitcounter/hitcounter.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-hitcounter',
+  templateUrl: './hitcounter.component.html',
+  styleUrls: ['./hitcounter.component.css']
+})
+export class HitcounterComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
